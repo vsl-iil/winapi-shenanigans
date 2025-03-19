fe9bdc5393b2e38c02573bd8fad25a54bf68214c
Егор Львов <workregor@mail.ru>
2025-03-19 22:41:28 +0300

    3D модели

--- demoscene/demo.cpp
+++ demoscene/demo.cpp
@@ -1,21 +1,30 @@
 // https://gargaj.github.io/demos-for-dummies/#graphics-meshload/1
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <algorithm>
 #include <Windows.h>
 #include <d3d11.h>
-#include <d3dcommon.h>
-#include <dxgi.h>
-#include <dxgiformat.h>
+#include <d3dcompiler.h>
+
 #define MINIAUDIO_IMPLEMENTATION
 #include "..\external\miniaudio.h"
 
+#define CGLTF_IMPLEMENTATION
+#include "..\external\cgltf.h"
+
 // а так можно было?
+// указываем, какие библиотеки компилятор должен слинковать. 
+// user32 отвечает за некоторые используемые функции WinAPI,
+// gdi32 - за отрисовку окна,
+// d3d11 - за взаимодействие с DirectX. 
 #pragma comment(lib,"user32.lib")
 #pragma comment(lib,"gdi32.lib")
 #pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"d3dcompiler.lib")
 
 bool gWindowWantsToQuit = false;
 
+// window procedure - процедура, обрабатывающая системные сообщения к окну (сиречь события)
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_KEYDOWN:
@@ -31,6 +40,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         case WM_SYSCOMMAND:
         {
             switch (wParam) {
+                // не даём выключить экран / уйти в скринсейвер
                 case SC_SCREENSAVE:
                 case SC_MONITORPOWER:
                     return 0;
@@ -44,10 +54,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
             break;
     }
 
+    // ответ по умолчанию
     return DefWindowProcA(hWnd, uMsg, wParam, lParam);
 }
 
 int main() {
+    // имя класса окна
     const char* CLASS_NAME = "DemosForDummies window class";
 
     WNDCLASSA windowClass;
@@ -116,15 +128,111 @@ int main() {
     if (device->CreateDepthStencilView(depthStencil, &descDSV, &depthStencilView) != S_OK) {
         return 6;
     }
+
+    CD3D11_RASTERIZER_DESC rasterizerDesc = CD3D11_RASTERIZER_DESC(CD3D11_DEFAULT());
+    rasterizerDesc.FrontCounterClockwise = true;
+    ID3D11RasterizerState* rasterizerState = NULL;
+    device->CreateRasterizerState(&rasterizerDesc, &rasterizerState);
+
+    ///////////////////////// CGLTF ////////////////////////
+
+    cgltf_options gltfOptions;
+    memset(&gltfOptions, 0, sizeof(cgltf_options));
+
+    cgltf_data* meshData = NULL;
+    const char* meshFilename = "Duck.glb";
+
+    cgltf_result res;
+    if ( (res = cgltf_parse_file(&gltfOptions, meshFilename, &meshData)) != cgltf_result_success ) {
+        return res+100;
+    }
+    if ( (res = cgltf_load_buffers(&gltfOptions, meshData, meshFilename)) != cgltf_result_success ) {
+        return -res-100;
+    }
+
+    cgltf_primitive& primitive = meshData->meshes[0].primitives[0];
+    D3D11_BUFFER_DESC bufferDesc = CD3D11_BUFFER_DESC((UINT)primitive.attributes[0].data->buffer_view->size, D3D11_BIND_VERTEX_BUFFER);
+    D3D11_SUBRESOURCE_DATA subData = { 0 };
+    subData.pSysMem = (char*)primitive.attributes[0].data->buffer_view->buffer->data + primitive.attributes[0].data->buffer_view->offset;
+    ID3D11Buffer* vertexBuffer = NULL;
+
+    if (device->CreateBuffer(&bufferDesc, &subData, &vertexBuffer) != S_OK) {
+        return 9;
+    }
+
+    D3D11_BUFFER_DESC ibBufferDesc = CD3D11_BUFFER_DESC((UINT)primitive.indices->buffer_view->size, D3D11_BIND_INDEX_BUFFER);
+    D3D11_SUBRESOURCE_DATA ibSubData = { 0 };
+    ibSubData.pSysMem = (char*)primitive.indices->buffer_view->buffer->data + primitive.indices->buffer_view->offset;
+    ID3D11Buffer* indexBuffer = NULL;
+    if (device->CreateBuffer(&ibBufferDesc, &ibSubData, &indexBuffer) != S_OK) {
+        return 10;
+    }
+
+    //////////////////////// SHADERS ////////////////////////
+
+    char shaderSource[] = R"(
+      struct VS_INPUT
+      {
+        float3 Pos: POSITION;
+      };
+
+      struct VS_OUTPUT
+      {
+        float4 Pos: SV_POSITION;
+      };
+      
+      VS_OUTPUT vs_main( VS_INPUT In )
+      {
+        VS_OUTPUT Out;
+        Out.Pos = float4( In.Pos, 1.0 );
+        return Out;
+      }
+
+      float4 ps_main( VS_OUTPUT In ): SV_TARGET
+      {
+        float4 c = 0;
+        return c;
+      }
+    )";
+
+    // vertex shader compile
+    ID3DBlob* vertexShaderBlob = NULL;
+    if (D3DCompile(shaderSource, strlen(shaderSource), NULL, NULL, NULL, "vs_main", "vs_5_0", 0, 0, &vertexShaderBlob, NULL) != S_OK) {
+        return 11;
+    }
+    ID3D11VertexShader* vertexShader = NULL;
+    if (device->CreateVertexShader(vertexShaderBlob->GetBufferPointer(), vertexShaderBlob->GetBufferSize(), NULL, &vertexShader) != S_OK) {
+        return 12;
+    }
+
+    // pixel shader compile
+    ID3DBlob* pixelShaderBlob = NULL;
+    if (D3DCompile(shaderSource, strlen(shaderSource), NULL, NULL, NULL, "ps_main", "ps_5_0", 0, 0, &pixelShaderBlob, NULL) != S_OK) {
+        return 13;
+    }
+    ID3D11PixelShader* pixelShader = NULL;
+    if (device->CreatePixelShader(pixelShaderBlob->GetBufferPointer(), pixelShaderBlob->GetBufferSize(), NULL, &pixelShader) != S_OK) {
+        return 14;
+    }
+
+    static D3D11_INPUT_ELEMENT_DESC inputDesc[] = {
+        {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+    };
+
+    ID3D11InputLayout* inputLayout = NULL;
+    if (device->CreateInputLayout(inputDesc, 1, vertexShaderBlob->GetBufferPointer(), vertexShaderBlob->GetBufferSize(), &inputLayout) != S_OK) {
+        return 15;
+    }
+
     ///////////////////////// AUDIO ////////////////////////
     ma_engine engine;
     if (ma_engine_init(NULL, &engine) != MA_SUCCESS) {
-        return 10;
+        return 20;
     }
 
     ma_sound sound;
     if (ma_sound_init_from_file(&engine, "audio.mp3", 0, NULL, NULL, &sound) != MA_SUCCESS) {
-        return 11;
+        return 21;
     }
 
     ma_sound_set_looping(&sound, MA_TRUE);
@@ -147,8 +255,43 @@ int main() {
         float blue  = (std::max)(0.0f, (std::min)(-(6.0f * (float)fabs((cursor/total)-0.67f) - 2.0f), 1.0f));
 
         const float clearColor[4] = {red, green, blue, 0.0f};
+        // const float clearColor[ 4 ] = { 0.1f, 0.2f, 0.3f, 0.0f };
         context->ClearRenderTargetView(backBufferView, clearColor);
         context->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+        // render model
+        // set viewport
+        const D3D11_VIEWPORT viewport = CD3D11_VIEWPORT(0.0f, 0.0f, (float)width, (float)height);
+        context->RSSetViewports(1, &viewport);
+
+        // render target - back buffer
+        context->OMSetRenderTargets(1, &backBufferView, depthStencilView);
+
+        // set shaders, rasterizer state & input layout
+        context->VSSetShader(vertexShader, NULL, 0);
+        context->PSSetShader(pixelShader, NULL, 0);
+
+        context->RSSetState(rasterizerState);
+        context->IASetInputLayout(inputLayout);
+
+        // set vertex buffer
+        ID3D11Buffer* buffers[] = { vertexBuffer };
+        const UINT stride[] = { (UINT)primitive.attributes[0].data->stride };
+        const UINT offset[] = { 0 };
+
+        context->IASetVertexBuffers(0, 1, buffers, stride, offset);
+
+        // set index buffer
+        DXGI_FORMAT format;
+        if (primitive.indices->component_type == cgltf_component_type_r_32u) {
+            format = DXGI_FORMAT_R32_UINT;
+        } else {
+            format = DXGI_FORMAT_R16_UINT;
+        }
+        context->IASetIndexBuffer(indexBuffer, format, 0);
+        context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+        context->DrawIndexed((UINT)primitive.indices->count, 0, 0);
 
         swapchain->Present(0, 0);
     }
@@ -156,10 +299,19 @@ int main() {
     ma_sound_stop(&sound);
     ma_engine_stop(&engine);
 
+    rasterizerState->Release();
     depthStencilView->Release();
     depthStencil->Release();
     backBufferView->Release();
     backBuffer->Release();
+    pixelShader->Release();
+    vertexShader->Release();
+    vertexBuffer->Release();
+    indexBuffer->Release();
+    inputLayout->Release();
+
+    cgltf_free(meshData);
+
     context->Release();
     device->Release();
 
